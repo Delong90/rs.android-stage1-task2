224a35551dd78cd1b283c3958454a5314148842b
Delong90 <matushkin030790@gmail.com>
2021-06-08 20:13:28 +0300

    feat: all task completed

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -1,9 +1,33 @@
 package subtask6
 
+
+//fun main(args:Array<String>) {
+//    var fibonacci = Fibonacci()
+//    println(fibonacci.productFibonacciSequenceFor(40))
+//}
+
 class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var fibonacciList = mutableListOf(0,1)
+        var i = 0
+        var j = 0
+        var intArray:IntArray = intArrayOf(0,0,0)
+        while (i==j){
+            var multiplication = fibonacciList[i]*fibonacciList[i+1]
+            if (n>multiplication){
+                fibonacciList.add(fibonacciList[i]+fibonacciList[i+1])
+                i++
+                j++
+            }else {
+                j++
+            }
+        }
+        intArray[0]=fibonacciList[i]
+        intArray[1]=fibonacciList[i+1]
+        if (fibonacciList[i]*fibonacciList[i+1] == n){intArray[2] = 1}
+        else{intArray[2] = 0}
+    return intArray
     }
 }
