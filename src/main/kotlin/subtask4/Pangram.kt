5c9689d7e9a5d3b66a7c1bb631796818c8ac5eea
Delong90 <jeka0307@mail.ru>
2021-06-08 13:04:53 +0300

    feat: task04 completed check inputString pangram or not

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,28 @@
 package subtask4
 
+
+fun main(args:Array<String>) {
+    var pangram = Pangram()
+    println(pangram.getResult("Sphinx of black quartz, judge my vow."))
+}
+
 class Pangram {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        var result = ""
+        var inputStringPangram = false
+        var charKonteiner = 'A'
+        var index = 0
+        while (charKonteiner <= 'Z') {
+            if (inputString.contains(charKonteiner, true)) {
+                index++
+            }
+            charKonteiner++
+        }
+        if (index==26) inputStringPangram = true
+
+        return result
+
     }
 }
